e88e8e1ee632eccadf6d4b5b846cb6e22db716eb
nfJaramillo <nf.jaramillo@uniandes.edu.co>
2019-11-21 18:44:26 -0500

    primer avance pagina inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mas_analitica/paginas/paginaInicial.dart';
 
 
 
 void main(){
-  runApp(new MaterialApp(home: null,)); // Corre el main y recibe un widget por parametro
+  runApp(new MaterialApp(home: new PaginaInicial(),)); // Corre el main y recibe un widget por parametro
   
 }
---
+++ lib/paginas/paginaInicial.dart
@@ -0,0 +1,53 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter/services.dart';
+import 'package:mas_analitica/UI/botonUI.dart';
+
+class PaginaInicial extends StatelessWidget{
+  @override
+
+    // Fuerza a la aplicacion a arracar en modo horizontal
+    Widget build(BuildContext context){
+    SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    
+    ]);
+
+    return new Material(color: Colors.yellow[700], // Un material es como una hoja de papel encima de la pantalla
+     child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Centra lo de la columna en el ejer y
+        children: <Widget>[
+          new Padding(
+            padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .05), // Añade espacio entre los textos
+            ),
+           FittedBox(fit:BoxFit.cover, child: new Text("+Analítica", style: TextStyle(fontSize: 100, fontWeight: FontWeight.bold, color: Colors.white))),
+            new Padding(
+            padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .05), // Añade espacio entre los textos
+            ),
+            Expanded(
+              child:new Row(
+            children: <Widget>[
+               new Padding(
+            padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .05), // Añade espacio entre los textos
+            ),
+              new BotonUI(Colors.purpleAccent[400], new Text("Lecturas",style: TextStyle(fontSize: 75,fontWeight: FontWeight.bold)), () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => null))), // Boton lecturas
+             new Padding(
+            padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .05), // Añade espacio entre los textos
+            ),
+              new BotonUI(Colors.purple[600], new Text("Preguntas",style: TextStyle(fontSize: 75, fontWeight: FontWeight.bold)), () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => null))), // Boton quiz
+              new Padding(
+            padding:  EdgeInsets.all(MediaQuery.of(context).size.width * .05), // Añade espacio entre los textos
+            ),
+            
+                  ],
+                  
+              ) ,
+            )
+          
+        ],
+      )
+    );
+  }
+}
