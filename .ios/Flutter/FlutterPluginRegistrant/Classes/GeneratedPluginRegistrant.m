ac95d9a693ad3e76880dda8adb658e59d98e6caa
Juan Sebastián Alegría Zúñiga <juanszalegria@gmail.com>
2019-12-08 14:30:58 -0500

    Extra-dependencies

---
+++ .ios/Flutter/FlutterPluginRegistrant/Classes/GeneratedPluginRegistrant.m
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <fluttertoast/FluttertoastPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+#import <share_extend/ShareExtendPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [FLTShareExtendPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTShareExtendPlugin"]];
+}
+
+@end
