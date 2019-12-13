0746a60949c66a8a8ec99b2cbf114c1fa5deb21d
nfJaramillo <nf.jaramillo@uniandes.edu.co>
2019-12-12 23:24:10 -0500

    Cambios en manifiestos y demas

--- .ios/Flutter/flutter_export_environment.sh
+++ .ios/Flutter/flutter_export_environment.sh
@@ -5,5 +5,5 @@ export "FLUTTER_APPLICATION_PATH=C:\Users\nicol\OneDrive - Universidad de los an
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NAME=2.0.0"
+export "FLUTTER_BUILD_NUMBER=2"
